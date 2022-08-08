59e81e1ce94fcb6702c1dcaf130dc4b328f52710
Sirfergar <82663775+Sirfergar@users.noreply.github.com>
2022-08-08 18:56:40 +0200

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,13 +1,15 @@
 import filtrar_por_seleccion
 import analisis_mestalla
 import analisis_seleccion
+import articulo
 
 import streamlit as st
 st.set_page_config(page_title="Análisis capacidad Mestalla", page_icon="📈")
 PAGES = {
+    "Artículo estudiando los datos": articulo,
     "Análisis de Mestalla": analisis_mestalla,
     "Análisis grandes Ligas": analisis_seleccion,
-    "Comparador estadios": filtrar_por_seleccion,
+    "Comparador estadios": filtrar_por_seleccion
 
 }
 st.sidebar.title('Analizador de asistencias')
